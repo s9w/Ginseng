c51362c53a5c000600e58157d7c46b62892d4c66
s9w <swerhausen@googlemail.com>
2014-11-28 12:03:17 +0100

    init upload

---
+++ init_data.js
@@ -0,0 +1,30 @@
+var init_data = {
+  "informations": [
+    {
+      "data":[
+        ["front", "my first front"],
+        ["back", "my first back"]
+      ],
+      "tags": [
+        "physik",
+        "mathe"
+      ],
+      "metadata":{
+        "created":"2014-11-27T09:43:46+01:00"
+      }
+    },
+
+    {
+      "data":[
+        ["front", "Apfel"],
+        ["back", "Apple"]
+      ],
+      "tags": [
+      ],
+      "metadata":{
+        "created":"2014-11-27T10:03:46+01:00"
+      }
+    }
+  ]
+}
+;
