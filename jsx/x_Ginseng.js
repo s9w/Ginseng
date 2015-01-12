f8c9711494c48475cb606e0664f0b04970392136
s9w <swerhausen@googlemail.com>
2015-01-12 18:19:13 +0100

    Now using the domain + readme fixes

--- jsx/x_Ginseng.js
+++ jsx/x_Ginseng.js
@@ -1,6 +1,6 @@
 var client = new Dropbox.Client({ key: "ob9346e5yc509q2" });
-//client.authDriver(new Dropbox.AuthDriver.Popup({receiverUrl: "https://s9w.github.io/ginseng/dropbox_receiver.html"}));
-client.authDriver(new Dropbox.AuthDriver.Popup({receiverUrl: "https://leastaction.org/ginseng/dropbox_receiver.html"}));
+client.authDriver(new Dropbox.AuthDriver.Popup({receiverUrl: "https://s9w.github.io/dropbox_receiver.html"}));
+//client.authDriver(new Dropbox.AuthDriver.Popup({receiverUrl: "https://leastaction.org/ginseng/dropbox_receiver.html"}));
 
 
 var Ginseng = React.createClass({
@@ -263,99 +263,6 @@ var Ginseng = React.createClass({
         );
     }
 });
-
-var Status = React.createClass({
-    getInitialState() {
-        return {
-            showOverwriteWarning: false,
-            now: moment()
-        };
-    },
-    componentDidMount: function() {
-        this.interval = setInterval(this.tick, 1000);
-    },
-    componentWillUnmount: function() {
-        clearInterval(this.interval);
-    },
-    tick: function() {
-        this.setState({now: moment()});
-    },
-    componentWillReceiveProps(){
-        this.setState({showOverwriteWarning: false});
-    },
-    onSaveClick(){
-        if(this.props.lastLoadedStr === "never"){
-            this.setState({showOverwriteWarning: true});
-        }else{
-            this.props.onDbSave();
-        }
-    },
-    onCancelOverwrite(){
-        this.setState({showOverwriteWarning: false});
-    },
-    render() {
-        var lastSavedStr  = this.props.meta.lastSaved;
-        var lastLoadedStr = this.props.lastLoadedStr;
-        if(this.props.meta.lastSaved !== "never"){
-            lastSavedStr = moment(this.props.meta.lastSaved).from(this.state.now);
-        }
-        if(this.props.lastLoadedStr !== "never"){
-            lastLoadedStr = moment(this.props.lastLoadedStr).from(this.state.now);
-        }
-        if(this.props.dropBoxStatus === "loading" ){
-            lastSavedStr = "...";
-            lastLoadedStr = "...";
-        }
-        if( this.props.dropBoxStatus === "saving"){
-            lastSavedStr = "...";
-        }
-
-        var popupOverwrite = false;
-        if(this.state.showOverwriteWarning) {
-            var buttonContainer =
-                <div className="flexRowDistribute" >
-                    <button
-                        onClick={this.props.onDbSave}
-                        className="button buttonGood">Yes</button>
-                    <button
-                        onClick={this.onCancelOverwrite}
-                        className="button buttonGood">Oh god no</button>
-                </div>;
-            popupOverwrite = <Popup
-                text="You're about to save to your Dropbox without loading first. This will overwrite previous data in your Dropbox! Continue?"
-                buttonContainer={buttonContainer}
-            />
-        }
-
-        return (
-            <div className="Status Component">
-                {popupOverwrite}
-                <section>
-                    <h3>Infos</h3>
-                    <div>Infos loaded: {this.props.dropBoxStatus === "loading"?"loading":this.props.infoCount}</div>
-                </section>
-
-                <section>
-                    <h3>Dropbox</h3>
-                    <div>{"Last save: " + lastSavedStr}</div>
-                    <div>{"Last load: " + lastLoadedStr}</div>
-                    <div>
-                        {this.props.dropBoxStatus === "initial" && <button
-                            className="buttonGood"
-                            onClick={this.props.onDBAuth}>Login</button>}
-                        {this.props.dropBoxStatus !== "initial" && <button
-                            disabled={this.props.dropBoxStatus !== "loggedIn"}
-                            onClick={this.props.onDbLoad}>Load</button>}
-                        {this.props.dropBoxStatus !== "initial" && <button
-                            disabled={!this.props.isChanged || this.props.dropBoxStatus !== "loggedIn"}
-                            onClick={this.onSaveClick}>Save</button>}
-                    </div>
-                </section>
-            </div>
-        )
-    }
-});
-
 
 var Editor = React.createClass({
     onChange(event){
